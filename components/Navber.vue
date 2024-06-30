74958c03ef2ea6149d77a281180c69e05ccefba0
我觉得很不妥 <ding169375@163.com>
2024-06-30 23:44:29 +0800

    Tailwind导航栏

--- components/Navber.vue
+++ components/Navber.vue
@@ -1,20 +1,37 @@
 <script setup lang="ts">
-  const current = ref('')
+const showSubMenu = ref(false)
+
+function toggleSubMenu() {
+  showSubMenu.value = !showSubMenu.value
+}
 </script>
 
 <template>
-  <div class="size-full">
-    <el-menu :default-active="current" mode="horizontal" justify-content="center">
-      <el-menu-item index="login">
-        User
-      </el-menu-item>
-      <el-sub-menu index="0">
-        <template #title>Workspace</template>
-        <el-menu-item index="1">Item 01</el-menu-item>
-        <el-menu-item index="2">Item 02</el-menu-item>
-      </el-sub-menu>
-    </el-menu>
-  </div>
+  <nav class="bg-gray-800 text-white py-4">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+      <div class="flex items-center">
+        <a href="/" class="text-xl font-bold">Platfrom</a>
+      </div>
+      <div class="flex items-center">
+        <nuxt-link to="/" class="px-4 py-2 hover:bg-gray-700 rounded-md"
+          :class="{ 'bg-gray-700': $route.path === '/' }">Home</nuxt-link>
+        <nuxt-link to="/login" class="px-4 py-2 hover:bg-gray-700 rounded-md"
+          :class="{ 'bg-gray-700': $route.path === '/login' }">Login</nuxt-link>
+        <div class="relative">
+          <button @click="toggleSubMenu" :class="{ 'bg-gray-700': $route.path.startsWith('/master') }"
+            class="px-4 py-2 hover:bg-gray-700 rounded-md flex items-center">
+            Master
+          </button>
+          <div v-if="showSubMenu" class="absolute z-10 bg-gray-800 rounded-md py-2 w-48 shadow-lg">
+            <nuxt-link to="/master01" :class="{ 'bg-gray-700': $route.path === '/master01' }"
+              class="block px-4 py-2 hover:bg-gray-700">Index</nuxt-link>
+            <nuxt-link to="/master01/slave-data" :class="{ 'bg-gray-700': $route.path === '/master01/slave-data' }"
+              class="block px-4 py-2 hover:bg-gray-700">slave-data</nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
@@ -22,4 +39,7 @@
   display: flex;
   justify-content: center;
 }
-</style>
+
+.relative:hover .hidden {
+  display: block;
+}</style>
---
+++ pages/master01/slave-data.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class=" text-green-400 text-4xl">congzhan shuju </h1>
+</template>
+
+<script lang="ts" setup>
+  
+</script>
+
+<style>
+  
+</style>
